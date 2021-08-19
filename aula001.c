a87552b9bb38d59f8f59e39accdcdb961da7f513
Larissa Carvalho <larissa.scsantos6@gmail.com>
2021-08-19 18:42:39 -0300

    aula001

---
+++ aula001.c
@@ -0,0 +1,22 @@
+// aula 001 - Histórico, printf, Hello World
+
+
+/* A linguagem C surgiu na década de 1970, inventada e implementada por Dennis Ritchie (1941-2011), usando sistema operacional Unix.
+
+Breve histórico e influências na criação da linguagem C:
+
+BCPL - Martin Richards
+B - Ken Thompson
+C - Dennis Ritchie
+
+*/
+
+// Imprimindo um texto na tela
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  printf("Hello World\n");
+  return 0;
+}
